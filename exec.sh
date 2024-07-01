2b66996a660b8de9d24221f02dd180c595a75315
tearfu1 <axuior12@gmail.com>
2024-07-01 18:01:49 +0300

    debug, exec.sh

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed "s/KEYCLOAK_HOST/keycloak.$(minikube ip).nip.io/" ingress-template.yml > 06_ingress.yml
-kubectl apply -f 01_namespace.yml -f 02_secrets.yml -f 03_storage.yml -f 04_postgres.yml -f 05_keycloak.yml -f 06_ingress.yml
+sed "s/KEYCLOAK_HOST/keycloak.$(minikube ip).nip.io/" ingress-template.yaml > 06_ingress.yaml
+kubectl apply -f 01_namespace.yaml -f 02_secrets.yaml -f 03_storage.yaml -f 04_postgres.yaml -f 05_keycloak.yaml -f 06_ingress.yaml
