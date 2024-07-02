84e3142ece0cb05e47b11e856615b868eb6a5d11
Take8ook <biryuk02@bk.ru>
2024-07-02 12:56:46 +0300

    fix: moving files to dir

--- exec.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed "s/KEYCLOAK_HOST/keycloak.$(minikube ip).nip.io/" ingress-template.yml > 06_ingress.yml
-kubectl apply -f 01_namespace.yml -f 02_secrets.yml -f 03_storage.yml -f 04_postgres.yml -f 05_keycloak.yml -f 06_ingress.yml
